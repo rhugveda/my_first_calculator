87f22af5c790b39248faff137f7c756a6538bebb
student <student>
2016-08-31 12:43:56 +0530

    addition

---
+++ Desktop/addition.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+main()
+{
+	float a,b;
+	printf("\nEnter first number: ");
+	scanf("%f",&a);
+	
+	printf("\nEnter second number: ");
+	scanf("%f",&b);
+
+	float sum=a+b;
+
+	printf("\nThe sum of %f and %f is %f .",a,b,sum);
+
+
+
+}
